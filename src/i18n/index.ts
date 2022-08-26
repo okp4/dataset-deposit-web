c9e1885b7b161a6daa7ad84a798095d9733562f0
lauren olivier <laurenzolivier@gmail.com>
2022-08-26 11:12:20 +0200

    feat: add translations files for stepper

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,11 @@
 import footer_en from './footer_en.json'
 import footer_fr from './footer_fr.json'
+import stepper_en from './stepper_en.json'
+import stepper_fr from './stepper_fr.json'
 
 export const translationsToLoad = [
-    { lng: 'en', namespace: 'footer', resource: footer_en },
-    { lng: 'fr', namespace: 'footer', resource: footer_fr }
-  ]
+  { lng: 'en', namespace: 'footer', resource: footer_en },
+  { lng: 'fr', namespace: 'footer', resource: footer_fr },
+  { lng: 'en', namespace: 'stepper', resource: stepper_en },
+  { lng: 'fr', namespace: 'stepper', resource: stepper_fr }
+]
