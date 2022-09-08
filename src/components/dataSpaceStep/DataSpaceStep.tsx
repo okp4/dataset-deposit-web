6c1ea8b767d8c776506d1f7a6052e79ebd4eb633
lauren olivier <laurenzolivier@gmail.com>
2022-09-08 10:43:51 +0200

    refactor: improve translations tree

--- src/components/dataSpaceStep/DataSpaceStep.tsx
+++ src/components/dataSpaceStep/DataSpaceStep.tsx
@@ -40,16 +40,20 @@ export const DataSpaceStep = ({
   return (
     <div className="okp4-dataspace-step-main">
       <Typography as="div" fontSize="small">
-        <div className="okp4-dataspace-step-description">{t('stepper:dataspace:description')}</div>
+        <div className="okp4-dataspace-step-description">
+          {t('stepper:dataset-deposit:steps:dataspace:description')}
+        </div>
       </Typography>
       <div className="okp4-dataspace-select-container">
         <Select
           fullWidth
           hasError={isInvalidStep}
-          helperText={isInvalidStep ? t('stepper:dataspace:not-selected') : ''}
+          helperText={
+            isInvalidStep ? t('stepper:dataset-deposit:steps:dataspace:not-selected') : ''
+          }
           onChange={handleDataSpaceSelection}
           options={options}
-          placeholder={t('stepper:dataspace:title')}
+          placeholder={t('stepper:dataset-deposit:steps:dataspace:title')}
           value={dataSpace}
         />
       </div>
