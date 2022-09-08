1e011eb79024d51260c0eb4f37171dbfdd93db32
Joey <joey.remion@gmail.com>
2022-09-08 08:54:20 +0200

    feat: add metadata step

--- src/components/stepper/Stepper.tsx
+++ src/components/stepper/Stepper.tsx
@@ -1,17 +1,36 @@
-import { Stepper as DataSetStepper, useTranslation } from '@okp4/ui'
+import { Stepper as DataSetStepper, truthy, useTranslation } from '@okp4/ui'
 import type { SelectValue, Step, UseState, UseTranslationResponse } from '@okp4/ui'
 import { useState } from 'react'
 import { DataSpaceStep } from '../dataSpaceStep/DataSpaceStep'
 import { UploadStep } from '../uploadStep/UploadStep'
+import type { Metadata } from '../metadataStep/MetadataStep'
+import { MetadataStep } from '../metadataStep/MetadataStep'
 
+// eslint-disable-next-line max-lines-per-function
 export const Stepper = (): JSX.Element => {
   const { t }: UseTranslationResponse = useTranslation()
   const [selectedDataSpace, setSelectedDataSpace]: UseState<SelectValue> = useState<SelectValue>('')
   const [dataSpaceStepError, setDataSpaceStepError]: UseState<boolean> = useState<boolean>(false)
+  const [metadata, setMetadata]: UseState<Metadata | undefined> = useState<Metadata | undefined>(
+    undefined
+  )
+  const [metadataStepError, setMetadataStepError]: UseState<boolean> = useState<boolean>(false)
 
   const checkDataSpaceSelection = (): boolean => {
     setDataSpaceStepError(!selectedDataSpace)
     return !!selectedDataSpace
+  }
+
+  const checkMetadata = (): boolean => {
+    const mandatoryMetadata =
+      !!metadata &&
+      truthy(metadata.datasetTitle) &&
+      truthy(metadata.author) &&
+      truthy(metadata.creator) &&
+      truthy(metadata.category) &&
+      truthy(metadata.licence)
+    setMetadataStepError(!mandatoryMetadata)
+    return mandatoryMetadata
   }
 
   const steps: Step[] = [
@@ -33,7 +52,10 @@ export const Stepper = (): JSX.Element => {
     },
     {
       label: t('stepper:dataset-deposit:steps:metadata:title'),
-      status: 'disabled'
+      content: (
+        <MetadataStep hasError={metadataStepError} metadata={metadata} onChange={setMetadata} />
+      ),
+      onValidate: checkMetadata
     },
     {
       label: t('stepper:dataset-deposit:steps:summary:title'),
