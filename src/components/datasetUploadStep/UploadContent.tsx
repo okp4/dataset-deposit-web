902a18721fbe115bfdc2bacac193650fdb41c524
Joey <joey.remion@gmail.com>
2022-10-20 10:05:30 +0200

    feat: add upload content in last step

--- src/components/datasetUploadStep/UploadContent.tsx
+++ src/components/datasetUploadStep/UploadContent.tsx
@@ -1,9 +1,3 @@
-import { ProgressBar } from '@okp4/ui'
-
 export const UploadContent = (): JSX.Element => {
-  return (
-    <div className="okp4-summary-step-main">
-      <ProgressBar />
-    </div>
-  )
+  return <div></div>
 }
